ffea3d04974638209479557c04b078fd40ed734d
Geoff Foote <gfoote@sritechnolgies.co.uk>
2017-05-04 20:35:07 +0100

    Fixing findAverage

--- src/test/scala/TestSparkAppTest.scala
+++ src/test/scala/TestSparkAppTest.scala
@@ -1,5 +1,5 @@
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSuite}
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession, Row}
 import TestSparkApp._
 
 
@@ -10,19 +10,18 @@ class TestSparkAppTest extends FunSuite with BeforeAndAfterEach with BeforeAndAf
 
   // Test data
   val url = getClass.getResource("/test.csv")
-  val path = url.toURI.getPath
+  val testFilePath = url.toURI.getPath
   val session = startSession
+
+  var testObject: TestSparkApp = _
 
   import session.implicits._
 
-  val testDS = session.sqlContext.read
-    .format("com.databricks.spark.csv")
-    .option("header", "true")
-    .load(path)
-    .as[InputData]
 
 
-  override def beforeEach() {
+  override def beforeEach(): Unit = {
+    session.sparkContext.setLogLevel("WARN")
+    testObject = new TestSparkApp(session)
   }
 
   override def afterEach() {
@@ -31,26 +30,67 @@ class TestSparkAppTest extends FunSuite with BeforeAndAfterEach with BeforeAndAf
   override protected def beforeAll(): Unit = {
   }
 
-  override protected def afterAll(): Unit = {}
-
-  test("Check test data") {
-    assert(testDS.count() === 3, "Data not loaded")
+  override protected def afterAll(): Unit = {
+    session.stop()
   }
 
-  test("testFilter") {
+  test("Load file") {
+    val result: Dataset[String] = testObject.loadFile(testFilePath)
 
+    val resultArray = result.collect()
+
+    val expected = Array(
+      "1,2,3",
+      "4,2,4",
+      "4,2,s",
+      "4,?,3",
+      "3,33,5",
+      "2,3,3",
+      "2,20,33")
+
+    assert(resultArray === expected," Load file")
   }
 
-  test("testFindAverage") {
 
+  test("test filter") {
+
+    val data: Dataset[String] = testObject.loadFile(testFilePath)
+
+    val result = testObject.filter(data).collect()
+
+    val expected = Array(
+      Row(1,2,3),
+      Row(4,2,4),
+      Row(3,33,5),
+      Row(2,3,3),
+      Row(2,20,33)
+    )
+    assert(result === expected, "Filter test data")
+  }
+
+  test("test findAverage") {
+
+    val data = testObject.loadFile(testFilePath)
+
+    val result = testObject.findAverage(data).collect()
+
+    val expected = Array(
+      Row(1,2,3,2.4),
+      Row(4,2,4,2.4),
+      Row(3,33,5, 2.4),
+      Row(2,3,3, 2.4),
+      Row(2,20,33, 2.4)
+      )
+
+    assert(result === expected," findAverage")
   }
 
   test("testStartSession") {
-
+    fail("Not implemented")
   }
 
   test("testFindAveragePerGroup") {
-
+    fail("Not implemented")
   }
 
 }
